8a9609e1a499dbf6fd7a7cbc4be2f8673da118df
SmithinJRaj <smithinjraj@email.com>
2026-05-02 21:34:40 +0530

    chore

---
+++ frontend/myApp/src/components/CustomButton.js
@@ -0,0 +1,19 @@
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+export default function CustomButton({ title, onPress }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#007bff',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  text: { color: '#fff', fontWeight: 'bold' },
+});
